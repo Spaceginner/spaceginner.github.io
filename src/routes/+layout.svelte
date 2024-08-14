72c5563b52480746ab0b85781c8953f6138f1e59
Spaceginner <ivan.demian2009@gmail.com>
2024-08-15 02:08:44 +0300

    change layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import "../app.css";
 </script>
 
-<div class="xl:max-w-[1280px] lg:mx-auto font-mono">
+<div class="xl:max-w-[1280px] lg:mx-auto font-mono min-h-screen">
   <slot />
+
+  <div class="absolute bottom-0 right-0 left-0 text-center p-5">
+    <p class="text-sm text-text dark:text-text-dark">love. piece. science.™</p>
+  </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,41 +7,39 @@
   import gal_logo from "$lib/assets/icons/gallery-icon.svg";
 </script>
 
-<div class="grid grid-cols-1 h-screen">
-  <div class="mt-5">
-    <h1 class="text-8xl text-text dark:text-text-dark major-mono-display-regular">Me, Ivan.</h1>
-    <p class="text-text dark:text-text-dark">a teenage enthusiast in science and programming.</p>
+<div class="mt-5 text-center">
+  <h1 class="text-8xl text-text dark:text-text-dark major-mono-display-regular">Me, Ivan.</h1>
+  <p class="text-text dark:text-text-dark">a teenage enthusiast in science and programming.</p>
+</div>
+
+<div class="my-12">
+  <div class="inline-flex items-center justify-center w-full mt-8">
+    <hr class="border bg-primary dark:bg-primary-dark h-1/2 my-4 w-full rounded">
+    <span class="absolute px-3 font-medium bg-background dark:bg-background-dark text-text dark:text-text-dark -translate-x-1/2 left-1/2">socials.</span>
   </div>
-  <hr class="border bg-primary dark:bg-primary-dark h-1 my-8 rounded">
-  <div>
-    <h2 class="text-7xl text-text dark:text-text-dark major-mono-display-regular">About.</h2>
-    <p class="text-text dark:text-text-dark">I'm a 15 years old highschool boy. Am in a relationship. Live in Kiev region, Ukraine.</p>
-
-    <div class="my-10"></div>
-
-    <h2 class="text-7xl text-text dark:text-text-dark major-mono-display-regular">Hobbies.</h2>
-    <p class="text-text dark:text-text-dark">Programming, Music Compositing, Photography.</p>
+  
+  <div class="flex flex-wrap lg:max-w-[1024px] lg:mx-auto justify-center">
+    <Link href="https://github.com/Spaceginner" desc="Github" icon={gh_logo}/>
+    <Link href="https://discord.com/users/565207149618135042" desc="Discord" icon={dc_logo}/>
+    <Link href="https://t.me/spaceginner" desc="Telegram" icon={tg_logo}/>
   </div>
-  <hr class="border bg-primary dark:bg-primary-dark h-1 my-8 rounded">
-  <div>
-    <div class="inline-flex items-center justify-center w-full">
-      <hr class="border bg-primary dark:bg-primary-dark h-1/2 my-4 w-full rounded">
-      <span class="absolute px-3 font-medium bg-background dark:bg-background-dark text-text dark:text-text-dark -translate-x-1/2 left-1/2">socials.</span>
-    </div>
 
-    <div class="flex flex-wrap lg:max-w-[1024px] lg:mx-auto justify-center">
-      <Link href="https://github.com/Spaceginner" desc="Github" icon={gh_logo}/>
-      <Link href="https://discord.com/users/565207149618135042" desc="Discord" icon={dc_logo}/>
-      <Link href="https://t.me/spaceginner" desc="Telegram" icon={tg_logo}/>
-    </div>
-
-    <div class="inline-flex items-center justify-center w-full">
-      <hr class="border bg-primary dark:bg-primary-dark h-1/2 my-4 w-full rounded">
-      <span class="absolute px-3 font-medium bg-background dark:bg-background-dark text-text dark:text-text-dark -translate-x-1/2 left-1/2">projects.</span>
-    </div>
-
-    <div class="flex flex-wrap lg:max-w-[1024px] lg:mx-auto justify-center">
-      <Link href="./gallery" desc="Gallery" icon={gal_logo}/>
-    </div>
+  <div class="inline-flex items-center justify-center w-full mt-12">
+    <hr class="border bg-primary dark:bg-primary-dark h-1/2 my-4 w-full rounded">
+    <span class="absolute px-3 font-medium bg-background dark:bg-background-dark text-text dark:text-text-dark -translate-x-1/2 left-1/2">projects.</span>
+  </div>
+  
+  <div class="flex flex-wrap lg:max-w-[1024px] lg:mx-auto justify-center">
+    <Link href="./gallery" desc="Gallery" icon={gal_logo}/>
   </div>
 </div>
+
+<hr class="border bg-primary dark:bg-primary-dark h-1 my-20 rounded">
+
+<h2 class="text-7xl text-text dark:text-text-dark major-mono-display-regular">About.</h2>
+<p class="text-text dark:text-text-dark">I'm a 15 years old highschool boy. Am in a relationship. Live in Kiev region, Ukraine.</p>
+
+<div class="my-10"></div>
+
+<h2 class="text-7xl text-text dark:text-text-dark major-mono-display-regular">Hobbies.</h2>
+<p class="text-text dark:text-text-dark">Programming, Music Compositing, Photography.</p>
